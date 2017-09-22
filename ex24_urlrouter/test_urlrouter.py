bdf2ca1b80d17ef10657cf945ef7f1ebae26bdb1
Zed A. Shaw <zed.shaw@gmail.com>
2017-09-22 19:05:12 -0400

    First few versions of routers working.

--- ex24_urlrouter/test_urlrouter.py
+++ ex24_urlrouter/test_urlrouter.py
@@ -1,4 +1,4 @@
-from urlrouter import TSTRouter
+from urlrouter import *
 
 
 def test_TSTRouter_add():
@@ -20,7 +20,10 @@ def test_TSTRouter_best_match():
     assert urls.best_match("NOTHING") == None
 
 def test_TSTRouter_match_all():
-    pass
+    urls = test_TSTRouter_add()
+    urls.add("/kiwikiwi/", "KiwiKiwi")
+    all = [n.value for n in urls.match_all("/kiwi")]
+    assert all == ["Kiwi", "KiwiKiwi"]
 
 def test_TSTRouter_match_shortest():
     urls = test_TSTRouter_add()
@@ -33,5 +36,41 @@ def test_TSTRouter_match_longest():
     urls.add("/kiwikiwi/", "KiwiKiwi")
     assert urls.match_longest("/k").value == "KiwiKiwi"
     assert urls.match_longest("/a").value == "Apple"
+
+
+def test_DictRouter_add():
+    urls = DictRouter()
+    urls.add("/kiwi/", "Kiwi")
+    urls.add("/apple/", "Apple")
+    return urls
+
+def test_DictRouter_exact_match():
+    urls = test_DictRouter_add()
+    assert urls.exact_match("/kiwi/") == "Kiwi"
+    assert urls.exact_match("/apple/") == "Apple"
+    assert urls.exact_match("/stuff/") == None
+
+def test_DictRouter_best_match():
+    urls = test_DictRouter_add()
+    assert urls.best_match("/kiwi/user/1234/").value == "Kiwi"
+    assert urls.best_match("/apple/user/2344/").value == "Apple"
+    assert urls.best_match("NOTHING") == None
+
+def test_DictRouter_match_all():
+    urls = test_DictRouter_add()
+    urls.add("/kiwikiwi/", "KiwiKiwi")
+    all = [n.value for n in urls.match_all("/kiwi/")]
+    assert all == ["Kiwi"]
+
+def test_DictRouter_match_shortest():
+    urls = test_DictRouter_add()
+    urls.add("/k/", "KiwiKiwi")
+    assert urls.match_shortest("/k/").value == "KiwiKiwi"
+
+def test_DictRouter_match_longest():
+    urls = test_DictRouter_add()
+    urls.add("/kiwikiwi/", "KiwiKiwi")
+    assert urls.match_longest("/kiwikiwi/").value == "KiwiKiwi"
+    assert urls.match_longest("/apple/").value == "Apple"
 
 
--- ex24_urlrouter/urlrouter.py
+++ ex24_urlrouter/urlrouter.py
@@ -1,5 +1,12 @@
 from tstree import TSTree
 
+class URLNode(object):
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+
 class URLRouter(object):
 
     def __init__(self):
@@ -18,10 +25,14 @@ class URLRouter(object):
         pass
 
     def match_shortest(self, url):
-        pass
+        key = lambda x: len(x.key)
+        possibles = sorted(self.match_all(url), key=key)
+        return possibles and possibles[0] or None
 
     def match_longest(self, url):
-        pass
+        key = lambda x: len(x.key)
+        possibles = sorted(self.match_all(url), key=key)
+        return possibles and possibles[-1] or None
 
 
 class TSTRouter(URLRouter):
@@ -39,11 +50,32 @@ class TSTRouter(URLRouter):
         return self.tree.find_part(url)
 
     def match_all(self, url):
-        pass
+        results = self.tree.find_all(url)
+        return results
 
-    def match_shortest(self, url):
-        return self.tree.find_shortest(url)
 
-    def match_longest(self, url):
-        return self.tree.find_longest(url)
+class DictRouter(URLRouter):
+
+    def __init__(self):
+        self.urls = {}
+
+    def add(self, url, value):
+        self.urls[url] = URLNode(url, value)
+
+    def exact_match(self, url):
+        node = self.urls.get(url)
+        return node and node.value or None
+
+    def best_match(self, url):
+        possible = sorted(self.match_all(url))
+        return possible and possible[0] or None
+        
+    def match_all(self, url):
+        results = []
+        for k,v in self.urls.items():
+            if url.startswith(k):
+                results.append(v)
+        return results
+
+
 
