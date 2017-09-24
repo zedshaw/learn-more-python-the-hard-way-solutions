b89cd72ab9931fdc7022b61d2bc9ab0cbd28cb34
Zed Shaw <zed.shaw+github@gmail.com>
2017-09-24 13:07:06 -0400

    Got some decent graphs with bokeh going.

--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@
 
 import numpy
 import sys
-import bokeh
+from bokeh import plotting
 import os
 
 command = sys.argv[1]
@@ -30,6 +30,8 @@ elif command == "init":
     if not os.path.exists(SAVE_DIR): os.mkdir(SAVE_DIR)
 
 elif command == "stats":
+    assert os.path.exists(SAVE_DIR), f"No save dir {SAVE_DIR}. In the right place?"
+
     # show the stats so far
     file_name = f"{SAVE_DIR}/{name}.npy"
     data = numpy.load(file_name)
@@ -39,11 +41,40 @@ elif command == "stats":
     for i in range(0, len(q25)):
         print("---")
         print(f"mean\t{mean[i]}\nstddev\t{stddev[i]}")
+        print(f"sd+/-1\t{mean[i] - stddev[i]}, {mean[i] + stddev[i]}")
+        print(f"sd+/-2\t{mean[i] - 2 * stddev[i]}, {mean[i] + 2 * stddev[i]}")
         print(f"q\t{q25[i]} {q50[i]} {q75[i]}\nIQR\t{q75[i] - q25[i]}")
 
 elif command == "graph":
-    # graph the runchart for the stat
-    pass
+    assert os.path.exists(SAVE_DIR), f"No save dir {SAVE_DIR}. In the right place?"
+    plotting.output_file(f"{SAVE_DIR}/{name}.html")
+    file_name = f"{SAVE_DIR}/{name}.npy"
+    data = numpy.load(file_name)
+
+    p = plotting.figure(title=f"stats for {name}", x_axis_label='x',
+                     y_axis_label='y')
+
+    for i in range(len(data[0,:])):
+        row = data[:,i]
+        p.line(range(0, len(row)), row, legend=f"S{i}", line_width=2)
+        means = [numpy.mean(row)] * len(row)
+        p.line(range(0, len(row)), means, legend=f"mean{i}", line_width=1, color="green")
+
+        sdplus1 = [numpy.mean(row) + numpy.std(row)] * len(row)
+        p.line(range(0, len(row)), sdplus1, legend=f"+-sd{i}", line_width=1, color="orange")
+
+        sdminus1 = [numpy.mean(row) - numpy.std(row)] * len(row)
+        p.line(range(0, len(row)), sdminus1, legend=f"+-sd{i}", line_width=1, color="orange")
+
+        sdplus2 = [numpy.mean(row) + numpy.std(row) * 2] * len(row)
+        p.line(range(0, len(row)), sdplus2, legend=f"+-sd{i}", line_width=1, color="red")
+
+        sdminus2 = [numpy.mean(row) - numpy.std(row) * 2] * len(row)
+        p.line(range(0, len(row)), sdminus2, legend=f"+-sd{i}", line_width=1, color="red")
+
+
+    plotting.show(p)
+
 else:
     print("USAGE: add stats or graph commands.")
 
