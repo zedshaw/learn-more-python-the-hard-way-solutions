7d8064cffa31a5745de52728da09e67d4f1114f7
Zed A. Shaw <zed.shaw@gmail.com>
2017-09-30 18:58:38 -0400

    Basic starter for hexdump.

---
+++ ex26_hexdump/hexdump.py
@@ -0,0 +1,25 @@
+def reshape(data, n):
+    results = []
+    while data:
+        results.append(data[:n])
+        del data[:n]
+
+    return results
+
+def format_bytes(inbytes):
+    byte_codes = inbytes.encode("utf-8")
+    shaped = reshape([b for b in byte_codes], 16)
+    return shaped
+
+def format(infile):
+    data = open(infile).read()
+    formatted = format_bytes(data)
+    count = 0
+    for row in formatted:
+        print(f"{count:010x} ", end="")
+        print(" ".join(f"{x:02x}" for x in row))
+        count += len(row)
+
+if __name__ == "__main__":
+    import sys
+    format(sys.argv[1])
---
+++ ex26_hexdump/test_hexdump.py
@@ -0,0 +1,25 @@
+import subprocess
+import hexdump
+
+
+
+def test_compare_output():
+    out = subprocess.run(["hexdump","test_hexdump.py"], check=True, stdout=subprocess.PIPE) 
+
+def test_reshape():
+    even = [1,2,3,4,5,6,7,8]
+    expect = [[1, 2], [3, 4], [5, 6], [7, 8]]
+    assert hexdump.reshape(even, 2) == expect
+
+    expect = [[1,2,3], [4,5,6], [7,8]]
+    assert hexdump.reshape(even, 3) == expect
+
+    odd = [1,2,3,4,5,6,7,8,9]
+    expect = [[1,2], [3,4], [5, 6], [7,8], [9]]
+    assert hexdump.reshape(odd, 2) == expect
+
+    expect = [[1,2,3], [4,5,6], [7,8,9]]
+    assert hexdump.reshape(odd, 3) == expect
+
+
+
