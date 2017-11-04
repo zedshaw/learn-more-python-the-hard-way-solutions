2f5d5c0f735884e44c49bfd5702842d96f65a237
Zed <zed.shaw@gmail.com>
2017-11-04 17:59:08 -0400

    Ex27 done.

--- ex27_tr/tr.py
+++ ex27_tr/tr.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python
 import sys
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--foo', help='foo help')
-args = parser.parse_args()
+def translate(from_c, to_c, in_string):
+    return in_string.replace(from_c, to_c)
 
-_, tr_from, tr_to = sys.argv
+def main(args, in_string):
+    print(">>>> args", args)
+    if args[0] == '-t':
+        # need to truncate first to len of last
+        to_c = args[2]
+        from_c = args[1][:len(to_c)]
+    else:
+        from_c = args[0]
+        to_c = args[1]
+    print("from_c", from_c, "to_c", to_c)    
+    return translate(from_c, to_c, in_string)
 
-
-for line in sys.stdin.readlines():
-    print(line.replace(tr_from, tr_to), end="")
+if __name__ == "__main__":
+    print(main(sys.argv[1:], sys.stdin.read()))
 
